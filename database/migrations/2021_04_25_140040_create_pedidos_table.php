91c148536fc37f41950cd560e1f389136c679e4d
martinquijano <mnquijanoalt@gmail.com>
2021-04-25 11:50:02 -0300

    Modelo y Migracion: Cliente, Pedido, Lista_pedido

---
+++ database/migrations/2021_04_25_140040_create_pedidos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePedidosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('cliente_id');
+            $table->date('fecha');
+            $table->string('estado');
+            $table->timestamps();
+
+            $table->foreign('cliente_id')->references('id')->on('clientes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pedidos');
+    }
+}
